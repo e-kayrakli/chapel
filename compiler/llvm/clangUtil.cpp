7f6cbe36bb11d7f8a89e93d09a18cfd8af3d4c75
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2023-08-28 10:51:17 -0700

    Fixes for bundled build
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- compiler/llvm/clangUtil.cpp
+++ compiler/llvm/clangUtil.cpp
@@ -88,8 +88,6 @@ using LlvmOptimizationLevel = llvm::PassBuilder::OptimizationLevel;
 #include "llvm/Support/CodeGen.h"
 #endif
 
-#define HAVE_LLVM_RV
-
 #ifdef HAVE_LLVM_RV
 #ifdef LLVM_USE_OLD_PASSES
 #include "rv/legacy/passes.h"
