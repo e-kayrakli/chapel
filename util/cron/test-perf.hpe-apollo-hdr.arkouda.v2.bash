bd23a615b416a8c666c3039b8f15d4478277b9f6
Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>
2026-01-12 10:04:29 -0600

    Add nobuild
    Signed-off-by: Engin Kayraklioglu <e-kayrakli@users.noreply.github.com>

--- util/cron/test-perf.hpe-apollo-hdr.arkouda.v2.bash
+++ util/cron/test-perf.hpe-apollo-hdr.arkouda.v2.bash
@@ -39,7 +39,7 @@ export GASNET_PHYSMEM_MAX="0.90"
 
 export CHPL_LLVM_GCC_PREFIX='none'
 
-nightly_args="${nightly_args} -no-buildcheck"
+nightly_args="${nightly_args} -nobuild -no-buildcheck"
 
 module list
 
